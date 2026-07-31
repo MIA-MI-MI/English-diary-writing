13247899ec08462d696d761172ea494a1fc3cdca
MIA-MI-MI <little-cats@coze.email>
2026-07-31 20:54:37 +0800

    1

--- english-diary/next.config.js
+++ english-diary/next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+}/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
+
+module.exports = nextConfig
 
 module.exports = nextConfig
